45bbdeca51d5dc52d223e02018c88abe8addc58c
algalochkin <contact@impresscms.org>
2008-11-17 18:58:04 +0000

    added
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartfaq/trunk@7031 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/russian/smartdbupdater.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+* $Id$ Russian translation. Charset: utf-8 (without BOM)
+* Module: SmartContent
+* Author: The SmartFactory <www.smartfactory.ca>
+* Licence: GNU
+*/
+if (!defined("XOOPS_ROOT_PATH")) { 
+ 	die("XOOPS root path not defined");
+}
+
+define("_SDU_IMPORT", "Импорт");
+define("_SDU_CURRENTVER", "Текущая версия: <span class='currentVer'>%s</span>");
+define("_SDU_DBVER", "Версия базы данных %s");
+define("_SDU_MSG_ADD_DATA", "Данные добавлены в таблицу %s");
+define("_SDU_MSG_ADD_DATA_ERR", "Ошибка во время добавления данных в таблицу %s");
+define("_SDU_MSG_CHGFIELD", "Изменение поля %s в таблице %s");
+define("_SDU_MSG_CHGFIELD_ERR", "Ошибка во время изменения поля %s в таблице %s");
+define("_SDU_MSG_CREATE_TABLE", "Таблица %s создана");
+define("_SDU_MSG_CREATE_TABLE_ERR", "Ошибка при создании таблицы %s");
+define("_SDU_MSG_NEWFIELD", "Добавлено поле %s");
+define("_SDU_MSG_NEWFIELD_ERR", "Ошибка при добавлении поля %s");
+define("_SDU_NEEDUPDATE", "Ваша база данных не обновлена. Пожалуйста, обновите таблицы Вашей базы данных!<br><b>Внимание : The SmartFactory strongly recommends you to backup all SmartSection tables before running this upgrade script.</b><br>");
+define("_SDU_NOUPDATE", "Ваша база данных обновлена. Обновления не требуются.");
+define("_SDU_UPDATE_DB", "Обновление базы данных");
+define("_SDU_UPDATE_ERR", "Ошибка во время обновления до версии %s");
+define("_SDU_UPDATE_NOW", "Обновить сейчас!");
+define("_SDU_UPDATE_OK", "Выполнено обновление до версии %s");
+define("_SDU_UPDATE_TO", "Обновление до версии %s");
+define("_SDU_UPDATE_UPDATING_DATABASE", "Обновление базы данных...");
+
+
+?>
